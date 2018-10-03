9c9818279e6ed3173e032229e8c5d84709d46b1f
Cassandra Matos <cassandra@matosmendez.com>
2018-10-03 13:22:21 -0700

    upload code base

---
+++ script.js
@@ -0,0 +1,96 @@
+//////////////////////////////////////////////////////////////////////////////
+// WHAT WILL THE CONSOLE DISPLAY?
+let temp = 95;
+let isHot;
+
+if(temp >= 85){
+    isHot = true;
+}
+else isHot = false;
+
+if(isHot){
+    console.log("Holy Moly it is burning up in here!");
+}
+else console.log("At least it's not hot.");
+//////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////
+// WHAT WILL THE CONSOLE DISPLAY?
+// var name = "Alejandro";
+
+// function sayHello(){
+//     name = "Cassandra";
+//     console.log(name);
+// }
+
+// console.log(name);
+// sayHello();
+//////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////
+// FILL IN THE BLANKS
+// We are trying to count the number of spaces are in the variable text.
+
+// let text = "This is a string to use for a JavaScript practice problem.";
+// let length = text.length;
+// let count = 0;
+
+// fill in the middle part of the for loop
+// for(var i=0; ;i++){
+    // fill in the paranthesis portion of the if statement
+//     if(){
+//         count++;
+//     }
+// }
+
+// fill in the paranthesis portion of the console.log to display the number of spaces counted
+// console.log();
+//////////////////////////////////////////////////////////////////////////////
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+// WHAT WILL THE CONSOLE DISPLAY?
+// var groceries = ["bananas", "peanut butter", "honey", "wheat bread", "strawberry jelly", "siracha"];
+// for(var i=0;i<groceries.length; i++){
+//     if(groceries[i]==="wheat bread"){
+//         groceries[i]="multi-grain bread";
+//     }
+// }
+// console.log(groceries[3]);
+//////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////
+// WHAT WILL THE CONSOLE DISPLAY?
+// for(var i=0; i<=5;i++){
+//     console.log(addFour(i));
+// }
+
+// function addFour(x){
+//     return x+4;
+// }
+//////////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////////////
+// WHAT WILL THE CONSOLE DISPLAY?
+// function test(x){
+//     if(x==1){
+//         return true;
+//     }
+//     else if(x==2){
+//         return false;
+//     }
+//     else{
+//         for(var i=2; i<x; i++){
+//             if(x%i==0){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+
+// }
+
+// console.log(test(37));
+//////////////////////////////////////////////////////////////////////////////
